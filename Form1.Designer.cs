73546e36d227ae8c453ffca5087150f18ffc4ceb
umair0s <umair0s@users.noreply.github.com>
2016-05-07 00:37:46 +0200

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace test__1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Butter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Milk";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(441, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Bread";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0.8 $";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1.15 $";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "1.00 $";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(141, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(123, 43);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(294, 81);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(115, 43);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(433, 81);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(109, 43);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(141, 143);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(400, 45);
+            this.richTextBox4.TabIndex = 9;
+            this.richTextBox4.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "In Bascket";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(566, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 42);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Add to Bascket";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(566, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 35);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Clear Bascket";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(144, 223);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(200, 53);
+            this.richTextBox5.TabIndex = 13;
+            this.richTextBox5.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Cost Breakdown";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(364, 221);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(177, 54);
+            this.richTextBox6.TabIndex = 15;
+            this.richTextBox6.Text = "";
+            this.richTextBox6.TextChanged += new System.EventHandler(this.richTextBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(484, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Total COst";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 311);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TEST BASCKET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Xml;
+using System.Net.WebSockets;
+using System.Net.Http;
+
+namespace test__1
+{
+    public partial class Form1 : Form
+    {
+        // declare variables here
+        double bu_price = 0.80 ;
+        double mlk_price = 1.15;
+        double br_price = 1.00;
+
+        double F_butter = 0.0;
+        double F_milk = 0.0;
+        //double F_bread = 0.0;
+        double bread_cost;
+
+
+        int q_butter = 0;
+        int q_milk = 0;
+        int q_bread = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AppendTextBox1(string value)    // position box invoked
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox1), new object[] { value });
+                return;
+            }
+
+
+            richTextBox1.Text = value;
+        }
+
+        public void AppendTextBox2(string value)    // position box invoked
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox2), new object[] { value });
+                return;
+            }
+
+
+            richTextBox2.Text = value;
+        }
+        public void AppendTextBox3(string value)    // position box invoked
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox3), new object[] { value });
+                return;
+            }
+
+
+            richTextBox3.Text = value;
+        }
+
+        public void AppendTextBox4(string value)    // position box invoked
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox4), new object[] { value });
+                return;
+            }
+
+
+            richTextBox4.Text = value;
+        }
+
+        public void AppendTextBox5(string value)    // position box invoked
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox5), new object[] { value });
+                return;
+            }
+
+
+            richTextBox5.Text += value;
+        }
+
+        public void AppendTextBox6(string value)    // position box invoked
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox6), new object[] { value });
+                return;
+            }
+
+
+            richTextBox6.Text += value;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // butter
+        {
+            q_butter++;
+            AppendTextBox1("Butter total Now: " + q_butter);
+        }
+        private void button2_Click(object sender, EventArgs e) // milk 
+        {
+            q_milk++;
+            AppendTextBox2("Milk total Now: " + q_milk);
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //  bread
+        {
+            q_bread++;
+            AppendTextBox3("Bread total Now: " + q_bread);
+        }
+
+        private void button4_Click(object sender, EventArgs e) // add to bascket
+        {
+            double pak0f4 = q_milk / 4;
+            double spare = q_milk % 4;
+
+            double butterpack = q_butter / 2;
+            double butterspare = q_butter % 2; 
+
+            AppendTextBox4("Butter : "+ q_butter + " ----  Milk : " + q_milk + " ----  Bread : " + q_bread+ "\r \n" );
+           
+
+            //logic
+
+            if (q_milk != 0 | q_milk == 0 )
+            {
+                //MessageBox.Show("Milk Present/n/r");
+                double q_milk_free = q_milk - (3 * pak0f4) - spare;
+                double x = q_milk - q_milk_free;
+                double milk_cost = x * mlk_price;
+                F_milk = milk_cost;
+                AppendTextBox5("Milk Cost:    "+ milk_cost+"\r");
+            }
+
+           if (q_butter != 0 | q_butter == 0)
+            {
+               
+                double butter_cost = q_butter * bu_price;
+                F_butter = butter_cost;
+                AppendTextBox5("Butter Cost:    " + butter_cost+"\r");
+            }
+
+            if (q_bread == 0)
+            {
+                bread_cost = q_bread * br_price;
+                AppendTextBox5("Bread Cost:    " + bread_cost + "\r");
+            }
+
+            if (q_bread == 1)
+            {
+                bread_cost = q_bread * br_price;
+                AppendTextBox5("Bread Cost:    " + bread_cost + "\r");
+            }
+            if (q_bread >1)
+            {
+                double u = q_bread - butterpack;
+                double disc_price = butterpack * 0.5;
+                double orig_price = u * br_price;
+                bread_cost = disc_price + orig_price;
+                AppendTextBox5("Bread Cost:    " + bread_cost + "\r");
+                double  Final_Cost = bread_cost + F_butter + F_milk;
+                AppendTextBox6(Final_Cost.ToString());
+            }
+
+            //if (q_bread <1 && butterpack ==0)
+            //{
+            //    double q_bread_discount = q_bread - butterpack;
+            //    double neu_q_bread = q_bread - q_bread_discount;
+            //    double bread_cost = (neu_q_bread * br_price) + (butterpack*0.5);
+            //    AppendTextBox5("Bread Cost:    " + bread_cost + "\r");
+            //    Double Final_Cost = bread_cost + F_butter + F_milk;
+            //    AppendTextBox6(Final_Cost.ToString());
+                
+
+            //}
+            //if (q_bread >=1 && butterpack ==0)
+            //{
+            //    bread_cost = q_bread * br_price;
+            //    AppendTextBox5("Bread Cost:    " + bread_cost + "\r");
+            //    Double Final_Cost = bread_cost + F_butter + F_milk;
+            //    AppendTextBox6(Final_Cost.ToString());
+            //}
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e) // clear
+        {
+            q_bread = 0;
+            q_butter = 0;
+            q_milk = 0;
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            richTextBox5.Clear();
+            richTextBox6.Clear();
+            F_butter = 0.0;
+            F_milk = 0.0;
+            bread_cost = 0.0;
+        }
+
+        private void richTextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
